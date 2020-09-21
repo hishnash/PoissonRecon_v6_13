2f19bf531714d95474034f68195ff0664d621580
matthaus woolard <matthaus.woolard@gmail.com>
2020-09-21 16:36:09 +1200

    update test

--- tests/PoisonReconTests/Tests.swift
+++ tests/PoisonReconTests/Tests.swift
@@ -4,21 +4,21 @@ import XCTest
 
 final class Tests: XCTestCase {
     func testRunWith3Points() {
-        let (f, v) = PoisonRecon._poisson_reconstruction(
+        let (f, v) = try! PoisonRecon.poisson_reconstruction(
             points: [
-                SIMD3<Float>([0, 0, 0, ]),
-                SIMD3<Float>([10, 0, 0, ]),
-                SIMD3<Float>([10, 0, 10, ]),
-                SIMD3<Float>([20, 0, 0, ]),
-                SIMD3<Float>([20, 0, 0, ]),
-                SIMD3<Float>([20, 0, 10, ]),
+                SIMD3([0, 0, 0, ]),
+                SIMD3([10, 0, 0, ]),
+                SIMD3([10, 0, 10, ]),
+                SIMD3([20, 0, 0, ]),
+                SIMD3([20, 0, 0, ]),
+                SIMD3([20, 0, 10, ]),
             ], normals: [
-                SIMD3<Float>([0, 1, 0 ]),
-                SIMD3<Float>([0, 1, 0 ]),
-                SIMD3<Float>([0, 1, 0 ]),
-                SIMD3<Float>([0, 1, 0 ]),
-                SIMD3<Float>([0, 1, 0 ]),
-                SIMD3<Float>([0, 1, 0 ]),
+                SIMD3([0, 1, 0 ]),
+                SIMD3([0, 1, 0 ]),
+                SIMD3([0, 1, 0 ]),
+                SIMD3([0, 1, 0 ]),
+                SIMD3([0, 1, 0 ]),
+                SIMD3([0, 1, 0 ]),
             ], scale: 100, samples_per_node: 5)
         print(f)
     }
